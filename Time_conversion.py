4e739140e7d78e6f14813e5f16afc27161fa0563
Natali☀️ <120851771+Cinderella-nv@users.noreply.github.com>
2023-06-07 00:50:40 +0300

    Time_conversion.py

--- Time_conversion.py
+++ Time_conversion.py
@@ -3,7 +3,8 @@
     ціле число
 Повертає: словник з ключами days, hours, minutes, seconds та їх відповідними значеннями'''
 
-
+#Функция конвертирует время и возвращает словарь с ключами: значениями - дни, часы, минуты, секунды
+#:param number - целочисленное число, коротое ввел пользователь
 def get_timing(number: int) -> dict:
 	convert = {"days": 0, "hours": 0, "minutes": 0, "seconds": 0}
 	day = get_days(number)
@@ -13,12 +14,15 @@ def get_timing(number: int) -> dict:
 	convert.update({"days": day[0], "hours": hour[0], "minutes": minute[0], "seconds": second})
 	return convert
 
+#Функция возвращает кортеж из: колличество дней и остаток в секундах
 def get_days(number: int) -> tuple:
 	return divmod(number, (60 * 60 * 24))
 
+##Функция возвращает кортеж из: колличество часов и остаток в секундах
 def get_hours(number: int) -> tuple:
 	return divmod(number, (60 * 60))
 
+##Функция возвращает кортеж из: колличество минут и остаток в секундах
 def get_minutes(number: int) -> tuple:
 	return divmod(number, 60)
 
