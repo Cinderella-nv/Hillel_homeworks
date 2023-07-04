8937d0f594fb6b86f8a55828c205428cb2c6fddd
Natali☀️ <120851771+Cinderella-nv@users.noreply.github.com>
2023-07-04 22:37:40 +0200

    Add files via upload

---
+++ Animal_Farm/animals/cat.py
@@ -0,0 +1,44 @@
+from .animal import Animal
+from random import randint
+
+
+class Cat(Animal):
+    """
+    Класс отвечает за симуляцию животного курица
+    """
+    def __init__(
+            self,
+            name: str,
+            age: int,
+            say_word='Мур-Мур'
+    ):
+        super().__init__(
+            name=name,
+            age=age,
+            say_word=say_word,
+            preferred_food={'кролик', 'курица'},
+            health=True
+        )
+        self.animal_type = 'Кошка'
+
+    def treat(self, hours: int) -> str:
+        """
+        Ухаживая за кошкой должное количество времени, мы получаем 10 кусь за палец
+        Если меньше - от 1 до 5 кусь
+        :param hours: сколько часов ухаживаем
+        :return: от 1 до 10 кусь
+        """
+        if hours > 2:
+            print(f'Вы ухаживаете за {self} {hours} часов и получаете десять кошачьих кусь')
+            return 'Клшачьих кусь: 10 шт.'
+        print(f'Вы ухаживаете за {self} {hours} часов и получаете немного кошачьих кусь')
+        return f'Кошачьих кусь: {randint(1, 5)}'
+
+    # def visit_a_doctor(self):
+    #     """
+    #     Метод реализует проверку здорова ли кошка и нужно ли ей к ветеринару
+    #     """
+    #     if self.health:
+    #         print(f'{self.animal_type} {self.name} только что от ветеринара и прекрасно себя чувствует!')
+    #     else:
+    #         print(f'{self.animal_type} {self.name} нуждается в визите к ветеринару!')
