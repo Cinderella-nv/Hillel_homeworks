73d94b7315f09c965ba887d9ed17812376e57fb4
Natali☀️ <120851771+Cinderella-nv@users.noreply.github.com>
2023-07-04 22:38:51 +0200

    Add files via upload

---
+++ Animal_Farm/animal_farm.py
@@ -0,0 +1,34 @@
+from animals import Dog, Hen, Cow, Cat
+from random import choices, choice, randint
+
+if __name__ == '__main__':
+    animals = [
+        Dog('Жучка', 3),
+        Hen('Коко', 2),
+        Cow('Милка', 5),
+        Cat('Клеопатра', 4)
+    ]
+
+    available_food = ['сено', 'трава', "зерно", "пшено", "каша", "мясо", "кость", "тортик", "кролик", "курица", "ягненок", "фасоль", "яблоко", "молоко", "кебаб", "клубника"]
+    heath_list = [True, False]
+    what_we_got = list()
+    what_we_lost = list()
+    for animal in animals:
+        animal.say()
+        """ то же самое, что и ниже
+        for i in range(3):
+            food = choice(available_food)
+            animal.eat(food)
+        """
+        for food in choices(available_food, k=3):
+            what_we_lost.append(food)
+            animal.eat(food)
+        if animal.hungry:
+            print(f'{animal} голодает! Покормите его.')
+        what_we_got.append(animal.treat(randint(0, 5)))
+        for health in choices(heath_list, k=1):
+            animal.health = health
+        animal.visit_a_doctor()
+        print('=' * 20)
+    print(f'Сегодня на ферме мы потратили: {", ".join(what_we_lost)} и получили {", ".join(what_we_got)}')
+
